da49bf43e21157aca82452a7a67ed0f25b75eef7
SebastianEdgarAbels <sebastian_e_a@yahoo.com>
2022-11-01 14:31:28 +0100

    Pagination done

--- Module_1/reactintro/src/components/BasicPagination.js
+++ Module_1/reactintro/src/components/BasicPagination.js
@@ -1,11 +1,12 @@
 import * as React from "react";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
+import "./BasicPagination.css";
 
 export default function BasicPagination({ getPaginationInput }) {
   // console.log("getPaginationInput :>> ", getPaginationInput);
   return (
-    <Stack spacing={2}>
+    <Stack spacing={2} className="pagination">
       <Pagination
         count={42}
         onChange={(e) => {
