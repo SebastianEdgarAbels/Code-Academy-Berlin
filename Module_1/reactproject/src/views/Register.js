51394e30a04512ffa3fa2a542f71730bd6629249
SebastianEdgarAbels <sebastian_e_a@yahoo.com>
2022-11-10 15:20:32 +0100

    started with Firebase

--- Module_1/reactproject/src/views/Register.js
+++ Module_1/reactproject/src/views/Register.js
@@ -1,7 +1,61 @@
-import React from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 function Register() {
-  return <div>Register u smartpants</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    console.log("email", e.target.value);
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    console.log("password", e.target.value);
+    setPassword(e.target.value);
+  };
+
+  const handleRegister = () => {
+    register(email, password);
+  };
+
+  return (
+    <>
+      <div>Register u smartpants</div>
+      <br />
+      <TextField
+        id="outlined-basic"
+        label="email"
+        variant="outlined"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <br />
+      <br />
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+        // autoComplete="current-password"
+      />
+      <br />
+      <br />
+
+      <TextField
+        id="outlined-password-input"
+        label="Confirm password"
+        type="password"
+        // autoComplete="current-password"
+      />
+      <br />
+      <br />
+      {/* <form action="" method="post"> */}
+      {/* <input type="submit" value="submit" /> */}
+      {/* </form> */}
+      <Button onClick={handleRegister}> Register</Button>
+    </>
+  );
 }
 
 export default Register;
